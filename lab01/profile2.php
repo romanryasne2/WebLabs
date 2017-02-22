14ce46121726363b48d262a5b51cd74995d10e57
romanryasne2 <roman.hubiak.pi.2015@lpnu.ua>
2017-02-22 11:06:46 +0200

    Add files via upload

---
+++ lab01/profile2.php
@@ -0,0 +1,105 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <link href="bootstrap.css" rel="stylesheet"/>
+    <script src="jquery.js"></script>
+    <script src="bootstrap.js"></script>
+</head>
+<body style="background-image: url('backimage.jpg'); background-repeat: no-repeat; background-size: cover">
+    <?php 
+        function getFirstName(){
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                return $_GET["firstName"];
+            }
+            else {
+                return $_POST["firstName"];
+            }
+        }
+
+        function getLastName(){
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                return $_GET["lastName"];
+            }
+            else {
+                return $_POST["lastName"];
+            }
+        }
+
+        function getMethod(){
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                return "GET";
+            }
+            else {
+                return "POST";
+            }
+        }
+
+        function getFileExtension(){
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                return pathinfo($_GET["fileName"], PATHINFO_EXTENSION);
+            }
+            else {
+                return pathinfo($_POST["fileName"], PATHINFO_EXTENSION);
+            }
+        }
+
+        function getCommand(){
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                return $_GET["command"];
+            }
+            else {
+                return $_POST["command"];
+            }
+        }
+
+        function getCountry(){
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                return $_GET["country"];
+            }
+            else {
+                return $_POST["country"];
+            }
+        }
+
+        function getEmail(){
+            if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+                return $_GET["email"];
+            }
+            else {
+                return $_POST["email"];
+            }
+        }
+    ?>
+    <div class="container">
+        <h3 class="text-center">Отримана інформація на іншому приймачі</h3>
+        <div class="col-xs-6">
+        <div>
+         <label>Ім'я: <?php echo getFirstName(); ?> </label>
+        </div>
+        <div>
+            <label>Прізвище: <?php echo getLastName(); ?> </label>
+        </div>
+        <div>
+            <label>Електронна пошта: <?php echo getEmail(); ?> </label>
+        </div>
+        <div>
+            <label>Країна: <?php echo getCountry(); ?> </label>
+        </div>
+        </div>
+        <div class="col-xs-6">
+        <div>
+            <label>Метод: <?php echo getMethod(); ?> </label>
+        </div>
+        <div>
+            <label>Назва програми: <?php echo getCommand(); ?> </label>
+        </div>
+        <div>
+            <label>Розширення файлу: <?php echo getFileExtension(); ?> </label>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
