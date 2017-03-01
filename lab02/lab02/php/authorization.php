0ecda60711d4962c5a22f2afe015bbe921c25d62
romanryasne2 <roman.hubiak.pi.2015@lpnu.ua>
2017-03-01 23:59:28 +0200

    Add files via upload

---
+++ lab02/lab02/php/authorization.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+    <link href="../libs/bootstrap.css" rel="stylesheet" />
+    <script src="../libs/jquery.js"></script>
+    <script src="../libs/bootstrap.js"></script>
+</head>
+<body style="background-image: url('../images/backimage.jpg'); background-repeat: no-repeat; background-size: cover">
+    <?php function authorize(){
+              if (($_POST["username"] == "admin") && $_POST["password"] == "admin"){
+                  return "Адміністратор";
+              }
+              if (($_POST["username"] == "student") && $_POST["password"] == "student"){
+                  return "Студент";
+              }
+              return "Не автентифікований";
+          }
+    ?>
+    <div class="container">
+        <h3 class="text-center">Клас користувача</h3>
+        <div>
+            <label class="text-center"> <?php echo authorize(); ?> </label>
+        </div>
+    </div>
+</body>
+</html>
