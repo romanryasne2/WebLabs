7c8064afc20597c060cf5481ed62a50b6b093de4
romanryasne2 <roman.hubiak.pi.2015@lpnu.ua>
2017-03-16 10:48:33 +0200

    Add files via upload

---
+++ lab04/lab04/js/scripts.js
@@ -0,0 +1,40 @@
+﻿function task1() {
+    $(".modal-body > p").text("Вітаємо на нашому сервісі!");
+    $("#my-dialog").modal("show");
+}
+
+var Number = 0;
+
+function task2() {
+    $("#files").prepend(`<div class="form-group">
+                            <label>Файл №${++Number}:</label>
+                            <input class="form-control" type="file" name="file${Number}"/>
+                        </div>`);
+}
+
+function task3() {
+    $(".modal-body > p").text("Відкрито після натискання по кнопці.");
+    $("#my-dialog").modal("show");
+}
+
+function showModal() {
+    $("#my-dialog").modal("show");
+}
+
+function task4() {
+    $("#first-name > h2").text("Вас звати " + $("input[name=firstName]").val() + ". Ми Вас вітаємо!");
+}
+
+function task5() {
+    $("#current-url > h2").text("Поточна сторінка " + window.location.href);
+}
+
+function changeImage() {
+    $("#image1").attr("src", $("#image1").attr("src").replace("image1", "image2"));
+    setTimeout(changeToPreviousImage, 1000);
+}
+
+function changeToPreviousImage() {
+    $("#image1").attr("src", $("#image1").attr("src").replace("image2", "image1"));
+    setTimeout(changeImage, 1000);
+}
---
+++ lab04/lab04/js/scripts3.js
@@ -0,0 +1,48 @@
+﻿function addButtons(sender) {
+    $(sender).removeAttr("onclick");
+
+    $("#buttons").append(`</br>
+                          <div class="col-xs-12 col-sm-4">
+                              <button class="btn btn-primary center-block" onclick="setEventHandlers()"> jQuery.Events</button>
+                          </div>`);
+
+    $("#buttons").append(`<div id="effects" class="col-xs-12 col-sm-4">
+                              <button class="btn btn-primary center-block">jQuery.Effects</button>
+                          </div>`);
+
+    $("#buttons").append(`<div id="css" class="col-xs-12 col-sm-4">
+                              <button class="btn btn-primary center-block">jQuery.CSS</button>
+                          </div>`);
+    $("#css").hide();
+}
+
+function setEventHandlers() {
+    $("#effects").on("click", doEffects);
+    $("#css").on("click", doCss);
+}
+
+function doEffects() {
+    $("#css").toggle();
+}
+
+function doCss() {
+    $("body").removeAttr("class");
+    $("body").css("background-color", `rgb(${Math.round(Math.random()*255)}, 
+                                           ${Math.round(Math.random()*255)}, 
+                                           ${Math.round(Math.random()*255)})`);
+}
+
+(function($) {
+    $.fn.reverseText = function() {
+        var text = $(this).val();
+        var reversedText = "";
+        for (var i = text.length - 1; i >= 0; i--) {
+            reversedText += text[i];
+        }
+        $(this).val(reversedText);
+    }
+})(jQuery);
+
+function reverseText() {
+    $("#text-field").reverseText();
+}
